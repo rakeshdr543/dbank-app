41a2ac5e8799ff33b5bb0d704dc16413f019c428
Rakesh D R <rakeshdr543@gmail.com>
2021-08-15 20:56:22 +0530

    Added token

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -4,18 +4,32 @@ pragma solidity >=0.6.0 <0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-  //add minter variable
+    //add minter variable
+    address public minter;
 
-  //add minter changed event
+    //add minter changed event
+    event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
-    //asign initial minter
-  }
+    constructor() public payable ERC20("Decentralized Bank Currency", "DCB") {
+        minter = msg.sender;
+        //asign initial minter
+    }
 
-  //Add pass minter role function
+    //Add pass minter role function
+    function passMinterRole(address dBank) public returns (bool) {
+        require(msg.sender == minter, "Only owner can pass change minter role");
+        minter = dbank;
 
-  function mint(address account, uint256 amount) public {
-    //check if msg.sender have minter role
-		_mint(account, amount);
-	}
+        emit MinterChanged(msg.sender, dbank);
+        return true;
+    }
+
+    function mint(address account, uint256 amount) public {
+        //check if msg.sender have minter role
+        require(
+            msg.sender == minter,
+            "Error, message.sender does not have minter role."
+        );
+        _mint(account, amount);
+    }
 }
